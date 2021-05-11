36fe8f71f5f732d9485dac62205e81a3d936cb8f
Lautaro <lautaroceballos2013@gmail.com>
2021-05-11 18:40:57 -0300

    Updates

---
+++ precache-manifest.7b6efc0c3e720dc00bed05e467e3c137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ea234a0e4342e0f96e04cb596660a5",
+    "url": "/-React--Proyecto-de-Blog%20/index.html"
+  },
+  {
+    "revision": "515cc601f0d2aeb95ded",
+    "url": "/-React--Proyecto-de-Blog%20/static/css/main.396c59ee.chunk.css"
+  },
+  {
+    "revision": "989761a2dbdddf5a900d",
+    "url": "/-React--Proyecto-de-Blog%20/static/js/2.d17ad459.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/-React--Proyecto-de-Blog%20/static/js/2.d17ad459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515cc601f0d2aeb95ded",
+    "url": "/-React--Proyecto-de-Blog%20/static/js/main.6f9d402e.chunk.js"
+  },
+  {
+    "revision": "5322214419b173f55dc7",
+    "url": "/-React--Proyecto-de-Blog%20/static/js/runtime-main.9449ad39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/-React--Proyecto-de-Blog%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
